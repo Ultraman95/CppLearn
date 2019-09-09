c008b1867f1fd2350d0f93e49444e165973223b1
shilf <tcl.shilf@gmail.com>
2019-09-09 14:50:18 +0800

    mkdir方法

--- base/src/main.cpp
+++ base/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #ifdef _WIN32
 	#include <windows.h>
+	#include <direct.h>
 #else
 	#include <unistd.h>
 #endif
@@ -9,9 +10,15 @@
 #include "gtest/gtest.h"
 
 #include "CLI/CLI.hpp"
+
+#include <stdio.h>
 using namespace std;
 
+#define MKDIR(a) _mkdir((a))
+
+void test();
 void testRAII();
+void testWriteFile();
 
 
 
@@ -20,14 +27,13 @@ int main(int argc, char** argv)
 	//CLI::App app;
 	//CLI11_PARSE(app, argc, argv);
 
-
+	//GoogleTest
 	testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
 #ifdef _MSC_VER
 	cout << "Window MSC_VER :" << _MSC_VER << std::endl;
 #endif
 	
-	testRAII();
 #ifdef _WIN32
 	Sleep(300000);
 #else
@@ -38,6 +44,12 @@ int main(int argc, char** argv)
     return 0;
 }
 
+
+void test() {
+	testRAII();
+	testWriteFile();
+}
+
 /**
  * 测试RAII，必须是指针变量
  */
@@ -45,6 +57,18 @@ void testRAII() {
 	CFloat* x = new CFloat("2.33333");
 	RAIIWrapper<CFloat> cf(x);
 
+}
+
+
+void testWriteFile() {
+	const char* dir = "../../data\\tick\\";  
+	int a = MKDIR(dir);		//子目录无法自动创建
+	const char* cc = "../../tt.csv";
+	FILE *file = fopen(cc, "a+");
+	if (file == NULL)
+	{
+		//fopen,a+会自动创建文件，但不会自动创建目录
+	}
 }
 
 
